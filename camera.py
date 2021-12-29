1700c3aac938c10e9d4f447f53b9d21744678062
fuzzyextractor <82440823+fuzzyextractor@users.noreply.github.com>
2021-12-29 19:40:03 +0800

    Add files via upload

--- camera.py
+++ camera.py
@@ -1,53 +1,79 @@
-import face_recognition
-import cv2
-import numpy as np
-import fuzzyextractor
-from multiprocessing import Pool
-import math
 import os
+import argparse
+import cv2
+import sys
+import numpy as np
+import insightface
+import fuzzyextractor
+import time
+import math
+from insightface.app import FaceAnalysis 
+from multiprocessing import Pool
 
+
+# This is a super simple (but slow) example of running face recognition on live video from your webcam.
+# There's a second example that's a little more complicated but runs faster.
+
+# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
+# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
+# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
+
+# Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
 
 
-n=128
-t=0.055
-FE=fuzzyextractor.FuzzyExtractor(n,t)
-
+n=512
+FE=fuzzyextractor.FuzzyExtractor(n,0.075)
 
 keys=[]
 helpers=[]
 names=[]
 pool=Pool(1)
 
-for f in os.listdir('.'):
-    if f[-3:]=='jpg' or f[-3:]=='png':
-        print(f)
-        image = face_recognition.load_image_file(f)
-        face_encoding = face_recognition.face_encodings(image)[0]
-        R,P=FE.Gen(face_encoding)
+
+
+parser = argparse.ArgumentParser(description='insightface app test') 
+parser.add_argument('--ctx', default=0, type=int, help='ctx id, <0 means using cpu')
+parser.add_argument('--det-size', default=640, type=int, help='detection size')
+args = parser.parse_args() 
+app = FaceAnalysis(name='antelopev2')
+app.prepare(ctx_id=args.ctx, det_size=(args.det_size,args.det_size))
+
+
+for f in os.listdir('./RPs/'):
+    if f[-3:]=='key':
+        name=f[0:-4]
+
+        with open('./RPs/'+name+'.key','rb') as fi:
+            R=fi.read(32)
+        with open('./RPs/'+name+'.helper','r') as fi:
+            P=np.array([int(x) for x in fi.readline().split()])
+
         keys.append(R)
         helpers.append(P)
-        names.append(f[0:-4])
-        proc=pool.apply_async(FE.Rep,(P,face_encoding))
+        names.append(name) 
 
-
-name = "Unknown"
+proc=pool.apply_async(FE.Rep,(P,np.zeros(n)))
+name='Unknown'
 idx=0
 cnt=0
+
 while True:
     # Grab a single frame of video
-    ret, frame = video_capture.read()
+    ret, img = video_capture.read()
+    try:
+        faces = app.get(img)
+    except:
+        continue
 
-    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-    rgb_frame = frame[:, :, ::-1]
+    rimg=img.copy()
 
-    # Find all the faces and face enqcodings in the frame of video
-    face_locations = face_recognition.face_locations(rgb_frame)
-    face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
+    for face in faces[0:1]:
+        img_embedding = face.normed_embedding.tolist()
+        box = face.bbox.astype(np.int)
+        color = (0, 0, 255)
+        cv2.rectangle(rimg, (box[0], box[1]), (box[2], box[3]), color, 2)
 
-    # Loop through each face in this frame of video
-    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
-        # See if the face is a match for the known face(s)
 
         if proc.ready():
             R_=proc.get()
@@ -61,15 +87,15 @@ while True:
                     name = "Unknown"
             idx=(idx+1)%len(names)
             print(idx)
-            proc=pool.apply_async(FE.Rep,(helpers[idx],face_encoding))
+            proc=pool.apply_async(FE.Rep,(helpers[idx],img_embedding)) 
 
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        cv2.rectangle(rimg, (box[0], box[3] + 35), (box[2], box[3]), (0, 0, 255), cv2.FILLED)
         font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+        cv2.putText(rimg, name, (box[0] , box[3] +30), font, 1.0, (255, 255, 255), 1)
+
 
     # Display the resulting image
-    cv2.imshow('Video', frame)
+    cv2.imshow('Video, Press Q to exit', rimg)
 
     # Hit 'q' on the keyboard to quit!
     if cv2.waitKey(1) & 0xFF == ord('q'):
---
+++ generate.py
@@ -0,0 +1,70 @@
+import os
+import argparse
+import cv2
+import sys
+import numpy as np
+import insightface
+import fuzzyextractor
+import time
+import math
+from insightface.app import FaceAnalysis 
+from multiprocessing import Pool
+
+
+
+n=512
+FE=fuzzyextractor.FuzzyExtractor(n,0.075)
+
+keys=[]
+helpers=[]
+names=[]
+pool=Pool(1)
+
+
+
+parser = argparse.ArgumentParser(description='insightface app test') 
+parser.add_argument('--ctx', default=0, type=int, help='ctx id, <0 means using cpu')
+parser.add_argument('--det-size', default=640, type=int, help='detection size')
+args = parser.parse_args() 
+app = FaceAnalysis(name='antelopev2')
+app.prepare(ctx_id=args.ctx, det_size=(args.det_size,args.det_size))
+
+
+print('generateing..')
+for f in os.listdir('./images/'):
+    if f[-3:]=='jpg' or f[-3:]=='png':
+        img = cv2.imread('./images/'+f)
+        try:
+            faces = app.get(img)
+        except:
+            continue
+
+        if len(faces)!=1:
+            continue
+        name=f[0:-4]
+        print('generateing.. '+name)
+        img_embedding = faces[0].normed_embedding.tolist() 
+        R,P=FE.Gen(img_embedding)  
+        keys.append(R)
+        helpers.append(P)
+        names.append(name) 
+
+        with open('./RPs/'+name+'.key','wb') as fo:
+            fo.write(R)
+
+        with open('./RPs/'+name+'.helper','w') as fo:
+            for x in P:
+                fo.write(str(x)+' ')
+
+
+        """
+        with open('./RPs/'+name+'.key','rb') as fi:
+            R2=fi.read(32)
+
+        P2=[]
+        with open('./RPs/'+name+'.helper','r') as fo:
+            P2=[int(x) for x in fo.readline().split()]
+        print(R==R2,P-P2)
+        """
+        
+print('done')
