351e4a741b17f12ab4e0bdc8c4ef58f9cac33b4a
fuzzyextractor <82440823+fuzzyextractor@users.noreply.github.com>
2021-12-29 21:39:10 +0800

    Add files via upload

--- camera.py
+++ camera.py
@@ -10,97 +10,99 @@ import math
 from insightface.app import FaceAnalysis 
 from multiprocessing import Pool
 
-
-# This is a super simple (but slow) example of running face recognition on live video from your webcam.
-# There's a second example that's a little more complicated but runs faster.
-
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
-
-# Get a reference to webcam #0 (the default one)
-video_capture = cv2.VideoCapture(0)
+if __name__=='__main__':
 
 
-n=512
-FE=fuzzyextractor.FuzzyExtractor(n,0.075)
+    # This is a super simple (but slow) example of running face recognition on live video from your webcam.
+    # There's a second example that's a little more complicated but runs faster.
 
-keys=[]
-helpers=[]
-names=[]
-pool=Pool(1)
+    # PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
+    # OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
+    # specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
 
-
-
-parser = argparse.ArgumentParser(description='insightface app test') 
-parser.add_argument('--ctx', default=0, type=int, help='ctx id, <0 means using cpu')
-parser.add_argument('--det-size', default=640, type=int, help='detection size')
-args = parser.parse_args() 
-app = FaceAnalysis(name='antelopev2')
-app.prepare(ctx_id=args.ctx, det_size=(args.det_size,args.det_size))
+    # Get a reference to webcam #0 (the default one)
+    video_capture = cv2.VideoCapture(0)
 
 
-for f in os.listdir('./RPs/'):
-    if f[-3:]=='key':
-        name=f[0:-4]
+    n=512
+    FE=fuzzyextractor.FuzzyExtractor(n,0.075)
 
-        with open('./RPs/'+name+'.key','rb') as fi:
-            R=fi.read(32)
-        with open('./RPs/'+name+'.helper','r') as fi:
-            P=np.array([int(x) for x in fi.readline().split()])
-
-        keys.append(R)
-        helpers.append(P)
-        names.append(name) 
-
-proc=pool.apply_async(FE.Rep,(P,np.zeros(n)))
-name='Unknown'
-idx=0
-cnt=0
-
-while True:
-    # Grab a single frame of video
-    ret, img = video_capture.read()
-    try:
-        faces = app.get(img)
-    except:
-        continue
-
-    rimg=img.copy()
-
-    for face in faces[0:1]:
-        img_embedding = face.normed_embedding.tolist()
-        box = face.bbox.astype(np.int)
-        color = (0, 0, 255)
-        cv2.rectangle(rimg, (box[0], box[1]), (box[2], box[3]), color, 2)
+    keys=[]
+    helpers=[]
+    names=[]
+    pool=Pool(1)
 
 
-        if proc.ready():
-            R_=proc.get()
-            if R_==keys[idx]:
-                print('Yes!')
-                name=names[idx]
-            else:
-                cnt+=1
-                if cnt>len(names)+1:
-                    cnt=0
-                    name = "Unknown"
-            idx=(idx+1)%len(names)
-            print(idx)
-            proc=pool.apply_async(FE.Rep,(helpers[idx],img_embedding)) 
 
-        cv2.rectangle(rimg, (box[0], box[3] + 35), (box[2], box[3]), (0, 0, 255), cv2.FILLED)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(rimg, name, (box[0] , box[3] +30), font, 1.0, (255, 255, 255), 1)
+    parser = argparse.ArgumentParser(description='insightface app test') 
+    parser.add_argument('--ctx', default=0, type=int, help='ctx id, <0 means using cpu')
+    parser.add_argument('--det-size', default=640, type=int, help='detection size')
+    args = parser.parse_args() 
+    app = FaceAnalysis(name='antelopev2')
+    app.prepare(ctx_id=args.ctx, det_size=(args.det_size,args.det_size))
 
 
-    # Display the resulting image
-    cv2.imshow('Video, Press Q to exit', rimg)
+    for f in os.listdir('./RPs/'):
+        if f[-3:]=='key':
+            name=f[0:-4]
 
-    # Hit 'q' on the keyboard to quit!
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+            with open('./RPs/'+name+'.key','rb') as fi:
+                R=fi.read(32)
+            with open('./RPs/'+name+'.helper','r') as fi:
+                P=np.array([int(x) for x in fi.readline().split()])
 
-# Release handle to the webcam
-video_capture.release()
-cv2.destroyAllWindows()
+            keys.append(R)
+            helpers.append(P)
+            names.append(name) 
+
+    proc=pool.apply_async(FE.Rep,(P,np.zeros(n)))
+    name='Unknown'
+    idx=0
+    cnt=0
+
+    while True:
+        # Grab a single frame of video
+        ret, img = video_capture.read()
+        try:
+            faces = app.get(img)
+        except:
+            continue
+
+        rimg=img.copy()
+
+        for face in faces[0:1]:
+            img_embedding = face.normed_embedding.tolist()
+            box = face.bbox.astype(np.int)
+            color = (0, 0, 255)
+            cv2.rectangle(rimg, (box[0], box[1]), (box[2], box[3]), color, 2)
+
+
+            if proc.ready():
+                R_=proc.get()
+                if R_==keys[idx]:
+                    print('Yes!')
+                    name=names[idx]
+                else:
+                    cnt+=1
+                    if cnt>len(names)+1:
+                        cnt=0
+                        name = "Unknown"
+                idx=(idx+1)%len(names)
+                print(idx)
+                proc=pool.apply_async(FE.Rep,(helpers[idx],img_embedding)) 
+
+            cv2.rectangle(rimg, (box[0], box[3] + 35), (box[2], box[3]), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(rimg, name, (box[0] , box[3] +30), font, 1.0, (255, 255, 255), 1)
+
+
+        # Display the resulting image
+        cv2.imshow('Video, Press Q to exit', rimg)
+
+        # Hit 'q' on the keyboard to quit!
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release handle to the webcam
+    video_capture.release()
+    cv2.destroyAllWindows()
--- generate.py
+++ generate.py
@@ -10,61 +10,61 @@ import math
 from insightface.app import FaceAnalysis 
 from multiprocessing import Pool
 
+if __name__=='__main__':
+
+    n=512
+    FE=fuzzyextractor.FuzzyExtractor(n,0.075)
+
+    keys=[]
+    helpers=[]
+    names=[]
+    pool=Pool(1)
 
 
-n=512
-FE=fuzzyextractor.FuzzyExtractor(n,0.075)
 
-keys=[]
-helpers=[]
-names=[]
-pool=Pool(1)
-
-
-
-parser = argparse.ArgumentParser(description='insightface app test') 
-parser.add_argument('--ctx', default=0, type=int, help='ctx id, <0 means using cpu')
-parser.add_argument('--det-size', default=640, type=int, help='detection size')
-args = parser.parse_args() 
-app = FaceAnalysis(name='antelopev2')
-app.prepare(ctx_id=args.ctx, det_size=(args.det_size,args.det_size))
+    parser = argparse.ArgumentParser(description='insightface app test') 
+    parser.add_argument('--ctx', default=0, type=int, help='ctx id, <0 means using cpu')
+    parser.add_argument('--det-size', default=640, type=int, help='detection size')
+    args = parser.parse_args() 
+    app = FaceAnalysis(name='antelopev2')
+    app.prepare(ctx_id=args.ctx, det_size=(args.det_size,args.det_size))
 
 
-print('generateing..')
-for f in os.listdir('./images/'):
-    if f[-3:]=='jpg' or f[-3:]=='png':
-        img = cv2.imread('./images/'+f)
-        try:
-            faces = app.get(img)
-        except:
-            continue
+    print('generateing..')
+    for f in os.listdir('./images/'):
+        if f[-3:]=='jpg' or f[-3:]=='png':
+            img = cv2.imread('./images/'+f)
+            try:
+                faces = app.get(img)
+            except:
+                continue
 
-        if len(faces)!=1:
-            continue
-        name=f[0:-4]
-        print('generateing.. '+name)
-        img_embedding = faces[0].normed_embedding.tolist() 
-        R,P=FE.Gen(img_embedding)  
-        keys.append(R)
-        helpers.append(P)
-        names.append(name) 
+            if len(faces)!=1:
+                continue
+            name=f[0:-4]
+            print('generateing.. '+name)
+            img_embedding = faces[0].normed_embedding.tolist() 
+            R,P=FE.Gen(img_embedding)  
+            keys.append(R)
+            helpers.append(P)
+            names.append(name) 
 
-        with open('./RPs/'+name+'.key','wb') as fo:
-            fo.write(R)
+            with open('./RPs/'+name+'.key','wb') as fo:
+                fo.write(R)
 
-        with open('./RPs/'+name+'.helper','w') as fo:
-            for x in P:
-                fo.write(str(x)+' ')
+            with open('./RPs/'+name+'.helper','w') as fo:
+                for x in P:
+                    fo.write(str(x)+' ')
 
 
-        """
-        with open('./RPs/'+name+'.key','rb') as fi:
-            R2=fi.read(32)
+            """
+            with open('./RPs/'+name+'.key','rb') as fi:
+                R2=fi.read(32)
 
-        P2=[]
-        with open('./RPs/'+name+'.helper','r') as fo:
-            P2=[int(x) for x in fo.readline().split()]
-        print(R==R2,P-P2)
-        """
-        
-print('done')
+            P2=[]
+            with open('./RPs/'+name+'.helper','r') as fo:
+                P2=[int(x) for x in fo.readline().split()]
+            print(R==R2,P-P2)
+            """
+            
+    print('done')
